32733e7c2cb71fe667b715ca36599ddfb90014f2
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2025-09-07 16:37:13 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,42 +2,45 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	static int [] arr = new int [9];
+	static int [] result = new int [7];
+	static boolean found = false;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Combination 사용해서 풀기
 		Scanner sc = new Scanner(System.in);
 		
-		int [] arr = new int[9];
-		int total = 0;
-		
-		
-		//9명 키 입력
-		
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
-			total += arr[i];
 		}
 		
-		// 0이라 설정하면 키가 0일수도있어서 음수값으로 초기설정
-		int fake1 = -1 , fake2 = -1;
-			
-		//2중 for문 으로 가짜 2명찾기 outer 사용해서 total 100 이면 한번에 2중 for문 나오기
-		outer:
-		for (int i = 0; i < 9; i++) {
-			for (int j = i+1; j < 9; j++) {
-				if (total - arr[i] - arr[j] == 100) {
-					fake1 = arr[i];
-					fake2 = arr[j];
-					break outer;
+		Combination(0,0);
+		
+		sc.close();
+	}
+	
+	
+	static void Combination(int idx,int depth) {
+		
+		if (found) return; //  정답 찾은 경우 중단
+		
+		if(depth == 7) { // 7명 뽑았을 때 (조합 완성)
+			int sum = 0;
+			for(int h : result) sum+= h;
+			if(sum==100) {
+				Arrays.sort(result);
+				for(int h : result) {
+					System.out.println(h);
 				}
-			} 
+				found = true;
+			}
+			return;
 		}
-		Arrays.sort(arr);
+		if(idx >= 9) return; //더 이상 뽑을 인덱스가 없을 때 중단
 		
-		for (int i = 0; i < 9; i++) {
-			if(arr[i] == fake1 || arr[i] == fake2) continue;
-			System.out.println(arr[i]);
-		}
+		result[depth] = arr[idx]; 
+	    Combination(idx + 1, depth + 1); // 선택
+	    Combination(idx + 1, depth);     // 선택 안함
 		
 	}
 
