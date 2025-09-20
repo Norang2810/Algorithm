dc0f1770a452f1386f98288188d6d67b717b1d72
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2025-09-20 12:32:50 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 188 ms, Memory: 14984 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= N; i++) {			
+			for (int j = 1; j <= N-i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
