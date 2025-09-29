965b4e1262b88dd69996a8ae991ad89ea0ce4e0a
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2025-09-29 23:14:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 584 ms, Memory: 46820 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,31 +1,50 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+class Member{
+	int age;
+	String name;
+	int order; //순서
+	
+	public Member(int age,String name,int order) {
+		this.age = age;
+		this.name = name;
+		this.order =order; // 순서
+	}
+}
 
+
+
+public class Main {
 	public static void main(String[] args) throws Exception {
+		 
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		
-		String[][] arr = new String[N][2];
-		
-		for(int i=0; i<N;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = st.nextToken();
-			arr[i][1] = st.nextToken();
-		}
-		
-		Arrays.sort(arr,(a,b) -> Integer.compare(Integer.parseInt(a[0]),Integer.parseInt(b[0])));
-		
-		StringBuilder sb = new StringBuilder();
+		List<Member> members = new ArrayList<>();
 		
 		for(int i=0;i<N;i++) {
-			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int age =Integer.parseInt(st.nextToken());
+			String name =st.nextToken();
+			members.add(new Member(age,name,i)); //i는 0~N 까지 반복이라
 		}
 		
+		members.sort((m1,m2) -> {
+			if(m1.age == m2.age) {
+				return Integer.compare(m1.order,m2.order);
+			}
+			return Integer.compare(m1.age, m2.age);
+		});
+		
+		
+		StringBuilder sb = new StringBuilder();
+		for(Member m : members) {
+			sb.append(m.age).append(" ").append(m.name).append("\n");
+		}
 		System.out.println(sb);
 	}
 
