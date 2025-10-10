1cd0cf62eafc8d3b89f42c9ad21e7a42de100f36
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2025-10-10 16:39:01 +0900

    [Gold V] Title: 회의실 배정, Time: 488 ms, Memory: 43976 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int time[][] = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			time[i][0] = Integer.parseInt(st.nextToken());
+			time[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(time, (a, b) -> {
+			if (a[1] == b[1]) {
+				return a[0] - b[0];
+			} else {
+				return a[1] - b[1];
+			}
+		});
+
+		int result = 0;
+		int endTime = 0;
+
+		for (int[] t : time) {
+			if (t[0] >= endTime) {
+				endTime = t[1];
+				result++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
