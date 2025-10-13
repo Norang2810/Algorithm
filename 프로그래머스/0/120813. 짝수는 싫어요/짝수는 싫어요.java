5a1f579a448411deb666bbdca39353551c438695
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2025-10-13 13:52:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.90 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List <Integer> oddList = new ArrayList<>();
+        
+        for(int i=1;i<=n;i++){
+            if(i % 2 != 0){
+                oddList.add(i);
+            }
+        }
+        
+        return oddList.stream().mapToInt(i->i).toArray();
+    }
+}
