330babe304c61ac186fd62ab1cdac294e88d33f8
박진우(Park Jinwoo) <84859429+Norang2810@users.noreply.github.com>
2026-04-16 09:56:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 200 ms, Memory: 20024 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,87 +1,83 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.StringTokenizer;
-/*
-V: 시작 노드 부터 DFS,BFS 통해 방문하게되는 노드를 순서대로 출력
-*/
-
+import java.util.*;
 
 public class Main {
-	static int N,M,V;
-	static int [] result ,visit;	
-	static ArrayList<Integer> [] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		N = Integer.parseInt(st.nextToken());
- 		M = Integer.parseInt(st.nextToken());
- 		V = Integer.parseInt(st.nextToken());
- 		
- 		arr = new ArrayList[N+1];
- 		visit = new int [N+1];
- 		
- 		for(int i=0;i<=N;i++) {
- 			arr[i] = new ArrayList<>();
- 		}
- 		
- 		for(int i=0; i<M;i++) {
- 			st = new StringTokenizer(br.readLine());
- 			
- 			int v = Integer.parseInt(st.nextToken());
- 			int u = Integer.parseInt(st.nextToken());
- 			arr[v].add(u);
- 			arr[u].add(v);
- 		}
- 		
- 		//정렬 list이므로 Collections 사용
- 		for(int i=1;i<=N;i++) {
- 			Collections.sort(arr[i]);
- 		}
- 		
- 		dfs(V);
- 		sb.append("\n");
- 		bfs(V);
- 		
- 		System.out.println(sb);
-	}
-	
-	public static void dfs(int start) {
-		//제귀 로직
-		visit[start] = 1;
-		sb.append(start).append(" ");
-		for(int next : arr[start]) {
-			if(visit[next] == 0 ) dfs(next);
-		} 
-	
-	}
-	
-	public static void bfs(int start) {
-		Deque<Integer> q = new ArrayDeque<>();
-		
-		Arrays.fill(visit, 0); //visit배열은 static선언이므로 메모리에 dfs에서 사용한게 남아있음 그래서 초기화
-		
-		q.add(start);
-		visit[start] =1;
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			sb.append(cur).append(" ");
-			
-			
-			for (int next: arr[cur]) {
-				if(visit[next] == 0) {
-					visit[next] =1;
-					q.add(next);
-				}
-			}
-		}
-	}
+
+    static int N, M, V;
+    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] Args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        //그래프 초기화하기
+
+        list = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<Integer>();
+        }
+
+        //간선 입력받기
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+
+            list[v1].add(v2);
+            list[v2].add(v1);
+        }
+
+        // 정렬
+        for (int i = 0; i < N + 1; i++) {
+            Collections.sort(list[i]);
+        }
+
+        // static  으로 선언해서 메모리에 남아있음 그래서 초기화
+        visited = new boolean[N + 1];
+        DFS(V);
+        sb.append("\n");
+
+        visited = new boolean[N + 1];
+        BFS(V);
+        System.out.println(sb);
+    }
+
+
+    static void DFS(int now) {
+        visited[now] = true;
+        sb.append(now).append(" ");
+
+        for (int next : list[now]) {
+            if (!visited[next]) {
+                DFS(next);
+            }
+        }
+    }
+
+    static void BFS(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            sb.append(now).append(" ");
+
+            for(int next :list[now]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
